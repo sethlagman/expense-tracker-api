ff3262b37c61db5a97f076329ccab4369fae8853
sethlagman <sethglagman@gmail.com>
2025-01-09 00:43:32 +0800

    Add search filter functionality

--- expense/api/views.py
+++ expense/api/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Expense
 from .serializer import ExpenseSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id', 'title', 'category', 'amount']
