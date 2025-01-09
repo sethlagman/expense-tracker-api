4cbf7e760147d9d933f965c89e1adbd87ce34c19
sethlagman <sethglagman@gmail.com>
2025-01-10 00:58:05 +0800

    Update filtering by date
    Change BooleanFilter to ChoiceFilter

--- expense/api/views.py
+++ expense/api/views.py
@@ -5,39 +5,35 @@ from .serializer import ExpenseSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
-from django_filters import FilterSet, BooleanFilter
+from django_filters import FilterSet, ChoiceFilter
 
 class ExpenseFilter(FilterSet):
-    pastweek = BooleanFilter(method='filter_by_past_week', label='Past Week')
-    pastmonth = BooleanFilter(method='filter_by_past_month', label='Past Month')
-    past_three_month = BooleanFilter(method='filter_by_past_three_month', label='Past Three Months')
+    CHOICES = [
+    ('option1', 'Past Week'),
+    ('option2', 'Past Month'),
+    ('option3', 'Past Three Months')
+    ]
+    date_filter = ChoiceFilter(choices=CHOICES, method='filter_by_date', label='Date Filter')
 
     class Meta:
         model = Expense
         fields = ['id', 'title', 'category', 'amount']
 
-    def filter_by_past_week(self, queryset, name, value):
+    def filter_by_date(self, queryset, name, value):
         pastweek_date =  str(date.today() - timedelta(days=7))
-
-        if value:
-            return queryset.filter(creation_date__gte=pastweek_date)
-        return queryset
-    
-    def filter_by_past_month(self, queryset, name, value):
         pastwmonth_date =  str(date.today() - relativedelta(months=1))
-
-        if value:
-            return queryset.filter(creation_date__gte=pastwmonth_date)
-        return queryset
-
-    def filter_by_past_three_month(self, queryset, name, value):
         past_three_month_date =  str(date.today() - relativedelta(months=3))
-
-        if value:
+        
+        if value == 'option1':
+            return queryset.filter(creation_date__gte=pastweek_date)
+        if value == 'option2':
+            return queryset.filter(creation_date__gte=pastwmonth_date)
+        if value == 'option3':
             return queryset.filter(creation_date__gte=past_three_month_date)
+        
         return queryset
 
-    
+
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
