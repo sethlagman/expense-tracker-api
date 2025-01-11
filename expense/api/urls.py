0634f1648f533e7d7c9ddbca0562240fc1ae3563
sethlagman <sethglagman@gmail.com>
2025-01-12 00:39:27 +0800

    Add custom DefaultRouter class
    For showing the other hyperlinks

--- expense/api/urls.py
+++ expense/api/urls.py
@@ -2,7 +2,25 @@ from django.urls import path, include
 from rest_framework import routers
 from api.views import ExpenseViewSet, UserRegistrationView, UserLoginView
 
-router = routers.DefaultRouter()
+class CustomDefaultRouter(routers.DefaultRouter):
+
+    def get_api_root_view(self, api_urls=None):
+        original_root_view = super().get_api_root_view(api_urls)
+
+        def custom_root_view(request, *args, **kwargs):
+            response = original_root_view(request)
+
+            additional_links = {
+                'register': 'http://127.0.0.1:8000/register/',
+                'login': 'http://127.0.0.1:8000/login/',
+            }
+            response.data.update(additional_links)
+            return response
+        
+        return custom_root_view
+
+
+router = CustomDefaultRouter()
 router.register(r'expense', ExpenseViewSet)
 
 urlpatterns = [
